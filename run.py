b550cef3b5556d2c4ff505652c91057e119b75d5
jmwania <josphatmwania237@gmail.com>
2019-03-15 13:51:16 +0300

    [20191503]

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+#from flask_restful import API
 
 app = Flask(__name__)
+#api = API(app) 
 
 orders = [
 {
@@ -16,13 +18,31 @@ orders = [
 ]
 
 @app.route('/', methods= ['GET'])
-def orders():
+def order():
     return make_response(jsonify(
             {
-            'status' : "ok",
+            'Status' : "ok",
             'Message' : "Sucess",
             'My orders': orders
-            }), 200)
+            }), 403)
+
+@app.route('/post', methods= ['POST'])
+def post_order():
+
+        data = request.get_json()
+        id = leng(orders) +1
+        item1 = data['item 1']
+        item2 = data['item 2']
+        price = data['price']
+
+
+payload = {
+'id': id,
+'Description': {'Item 1': item1, 'Item 2': item2},
+'Price': price
+}
+orders.append(payload)
+
 
 
 if __name__ == '__main__':
